4285e7215b6c5de6da24e907265b8c3853316a03
Tomoyuki Hata <7702653+hata6502@users.noreply.github.com>
2021-09-25 13:14:13 +0900

    runScrapboxTwitterShare

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,37 @@
 import twitter from "twitter-text";
 
-// @ts-expect-error The type is not defined.
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-scrapbox.PageMenu.addMenu({
-  title: "Share to Twitter",
-  image: "https://i.gyazo.com/a4dfaf020789cbf745fa5c916e3a107e.png",
-  onClick: () => {
-    // @ts-expect-error The type is not defined.
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-    const content = scrapbox.Page.lines
+const runScrapboxTwitterShare = (): void => {
+  // @ts-expect-error The type is not defined.
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+  scrapbox.PageMenu.addMenu({
+    title: "Share to Twitter",
+    image: "https://i.gyazo.com/a4dfaf020789cbf745fa5c916e3a107e.png",
+    onClick: () => {
       // @ts-expect-error The type is not defined.
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      .map((line) => line.text as string)
-      .join("\n") as string;
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+      const content = scrapbox.Page.lines
+        // @ts-expect-error The type is not defined.
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        .map((line) => line.text as string)
+        .join("\n") as string;
 
-    const fullText = `${location.href}
+      const fullText = `${location.href}
 ${content}`;
 
-    const parsedTweet = twitter.parseTweet(fullText);
+      const parsedTweet = twitter.parseTweet(fullText);
 
-    const text = fullText.slice(
-      parsedTweet.validRangeStart,
-      parsedTweet.validRangeEnd
-    );
+      const text = fullText.slice(
+        parsedTweet.validRangeStart,
+        parsedTweet.validRangeEnd
+      );
 
-    open(
-      `https://twitter.com/intent/tweet?${new URLSearchParams({
-        text,
-      }).toString()}`
-    );
-  },
-});
+      open(
+        `https://twitter.com/intent/tweet?${new URLSearchParams({
+          text,
+        }).toString()}`
+      );
+    },
+  });
+};
+
+export { runScrapboxTwitterShare };
